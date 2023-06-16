8432247f5367bcf7dfac56afbfe980501f73cba3
Remaster <borov704@gmail.com>
2023-06-16 06:57:43 +0200

    First commit

--- js/index.js
+++ js/index.js
@@ -2,8 +2,9 @@ let wind = document.querySelector("#rain-speed")
 let city = document.querySelector("#city")
 let temp = document.querySelector("#temp") 
 let humidity = document.querySelector('#humidity-value')
+let api_key = ""
 navigator.geolocation.getCurrentPosition(position => {
-    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=e484b44a91dfa6d50e68752af149a752&units=metric`
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${api_key}&units=metric`
     fetch(url)
     .then(
         response => response.json()
